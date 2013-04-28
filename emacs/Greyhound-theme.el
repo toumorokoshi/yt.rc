94c85ad043bba0bec440521241fa9676bfba42a3
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-04-28 07:13:53 +0200

    Adding new background macros

--- emacs/Greyhound-theme.el
+++ emacs/Greyhound-theme.el
@@ -1,17 +1,22 @@
 ;; For reference
 (setq ght/tronyellow "#F9E30B")
 (setq ght/tronblue "#81E7F8")
+(setq ght/gray-black "#444444")
+(setq ght/black "#000000")
 
 
 (deftheme Greyhound
   "Created 2013-04-24.")
 (let ((special-fg '(:foreground ,ght/tronyellow))
-      (special-second-fg '(:foreground ,ght/tronblue)))
+      (special-second-fg '(:foreground ,ght/tronblue))
+      (special-bg '(:background ,ght/gray-black))
+      (special-second-bg '(:background ,ght/black))
+     )
 
 (custom-theme-set-faces
  'Greyhound
  '(cursor ((t (:background "#AAAAAA" :foreground "#f6f3e8"))))
- '(region ((t (:background "#444444" :foreground "#f6f3e8"))))
+ '(region ((t (,@special-bg :foreground "#f6f3e8"))))
  '(mode-line ((t (:background "#444444" :foreground "#FFE64D" :box (:color "#444444" :line-width 6)))))
  '(mode-line-inactive ((t (:background "#444444" :foreground "#6FC3DF" :box (:color "#444444" :line-width 6)))))
  '(vertical-border ((t (:foreground "#CCC"))))
@@ -32,10 +37,10 @@
  '(button ((t (:background "#444444" :foreground "#f6f3e8" :underline t :weight bold))))
  '(header-line ((t (:background "#303030" :foreground "#e7f6da"))))
  ;; faces for powerline
- '(powerline-active1 ((t (,@special-fg :background "#000000" :inherit mode-line))))
- '(powerline-active2 ((t (,@special-second-fg :background "#444444" :inherit mode-line))))
- '(powerline-inactive1 ((t (:background "#000000" :inherit mode-line-inactive))))
- '(powerline-inactive2 ((t (:background "#444444" :inherit mode-line-inactive))))
+ '(powerline-active1 ((t (,@special-fg ,@special-second-bg :inherit mode-line))))
+ '(powerline-active2 ((t (,@special-second-fg ,@special-bg :inherit mode-line))))
+ '(powerline-inactive1 ((t (,@special-second-bg :inherit mode-line-inactive))))
+ '(powerline-inactive2 ((t (,@special-bg :inherit mode-line-inactive))))
  '(default ((t (:background "#171717" :foreground "#f6f6f6")))))
 )
 
