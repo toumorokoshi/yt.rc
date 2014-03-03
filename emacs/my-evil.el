3ef7731332b321eebfab06e82a1f1bea527a7ca9
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2014-03-03 15:21:16 -0800

    adding some stuff

--- emacs/my-evil.el
+++ emacs/my-evil.el
@@ -2,3 +2,5 @@
 ; bringing back emacs keybindings
 (define-key evil-visual-state-map "\C-w" 'kill-region)
 (define-key evil-normal-state-map "\C-t" 'transpose-chars)
+(define-key evil-normal-state-map "\C-p" 'previous-line)
+(define-key evil-normal-state-map "\C-n" 'next-line)
